9ef122dc97c8858a73de286cace8b51a1887206e
Bartek <bartekd001@gmail.com>
2021-03-15 19:30:23 +0100

    Updates

---
+++ precache-manifest.3c04b555bd1b0c1273d420c1824842ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "55a2b8089316850f0ee4",
+    "url": "/card/static/css/main.71dc97c0.chunk.css"
+  },
+  {
+    "revision": "55a2b8089316850f0ee4",
+    "url": "/card/static/js/main.7c0d539f.chunk.js"
+  },
+  {
+    "revision": "ea07af475a06bddebb02",
+    "url": "/card/static/js/runtime~main.3c9c83e0.js"
+  },
+  {
+    "revision": "a1fd85a181fb6bbdb394",
+    "url": "/card/static/js/2.46254229.chunk.js"
+  },
+  {
+    "revision": "52830b1b28152198f5920421f5f0f6b0",
+    "url": "/card/static/media/smile.52830b1b.svg"
+  },
+  {
+    "revision": "768307dd5b6d91d320a23d6928d0f573",
+    "url": "/card/index.html"
+  }
+];
